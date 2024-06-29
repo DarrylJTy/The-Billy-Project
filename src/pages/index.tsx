ade97bf758c622f099d837cb181971f5306b859a
Hans Jared Sescon <hjcsescon@gmail.com>
2024-06-29 19:02:15 +0800

    Buttons implemented
    - added image to index
    - created functioning buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,14 @@ import * as React from "react"
 import Layout  from "../components/Layout"
 import "../style/global.css"
 import "../style/index.css"
-import { Container } from "react-bootstrap"
+import { Button, Container } from "react-bootstrap"
+import { Link } from "gatsby"
 
 export default function Home() {
   return (
     <Layout>
         <Container fluid className="index-container">
           <div className="content-container">
-
             <div className='d-flex flex-row first-content-block'>
               <div className="col-sm text-area">
                 <div className="text-content">
@@ -19,22 +19,46 @@ export default function Home() {
                   </p>
                 </div>
               </div>
-              <div className="col-sm img-area">
-                hi
-              </div>
 
-            </div>
-
-            <div className='d-flex flex-row'>
-              <div className='col-sm branches-block'>
-                hello
-
-              </div>
-              <div className='col-sm review-block'>
-                afsf
+              <div className="col-sm px-0 img-area">
+                <img src="../living-space.png" className="img-fluid image1"/>
+                <Link to="/tiles" className="btn btn-outline-dark button1">
+                  shop now
+                </Link>
               </div>
             </div>
 
+            <div className='d-flex flex-row second-content-block'>
+              <div className="col-sm branches-area">
+                <div className="branches-content">
+                  <p>
+                    <h2>Find a Store Near You</h2><br />
+                    Check out our branches located in Laguna and Metro Manila. 
+                  </p>
+                  <Link to="/branches" className="btn btn-outline-dark button2">
+                    branches
+                  </Link>
+                </div>
+              </div>
+
+              <div className="col-sm review-area">
+                <div className="review-content">
+                  <p>
+                    <h2>Shopped Recently?</h2><br />
+                    Let us know how well we served you by rating us on our Facebook Page.
+                  </p>
+                  <a
+                    href="https://www.facebook.com/mrbillybuildeasy"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-outline-dark button3"
+                  >
+                    Review Us
+                  </a>
+                </div>
+              </div>
+            </div>
+            
           </div>
         </Container>
     </Layout>
