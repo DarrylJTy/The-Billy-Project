462719bf3663a471f0433f47a36fd08d66d4d5a8
notredoo <128708787+notredoo@users.noreply.github.com>
2024-07-21 12:21:29 +0800

    Feature branches page (#4)
    * Progress on branches and store page
    
    - implemented carousel view on branches
    - created display for store product page
    
    * created branches
    
    - compiled pages into catalog
    - added custom footer
    - base code for branches created
    
    * test commit
    
    - testin pull request
    
    * Finished Adding branch details
    
    - added pictures
    - added branch details
    - added button that links to google maps of each branch
    
    ---------
    
    Co-authored-by: DerrickVal1256 <114970923+DerrickVal1256@users.noreply.github.com>

--- src/pages/branches.tsx
+++ src/pages/branches.tsx
@@ -11,34 +11,84 @@ export default function branches() {
     <Layout>
         <Container fluid className="branches-container">
           <div className="content-container">
-            <div className='d-flex flex-row branches-content-block'>
-              <Carousel>
+              <Carousel className='d-flex flex-row branches-content-block'>
                 <Carousel.Item>
-                  <img src="../living-space.png" />
+                  <img src="../laspinas-branch.png" />
                   <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h3>Las Pinas Branch</h3>
+                    <p className='branch-text'>Marcos Alvarez Ave, Las Piñas, 1747 Metro Manila</p>
+                    <a
+                      href="https://maps.app.goo.gl/bwhqiJ5te4M6PYby5"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn btn-outline-light"
+                    >
+                      View on Google Maps
+                    </a>
                   </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img src="../living-space.jpg" />
+                  <img src="../c6-branch.png" />
                   <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h3>C6 Branch</h3>
+                    <p className='branch-text'>BLK 35, Sta. Ana  Lupang Arienda, Taytay Rizal</p>
+                    <a
+                      href="https://maps.app.goo.gl/sRABRcs91VakmreX9"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn btn-outline-light"
+                    >
+                      View on Google Maps
+                    </a>
                   </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img src="../mrbillylogorevamp.png" />
+                  <img src="../floodway-branch.png" />
                   <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                      Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
+                    <h3>Floodway Branch</h3>
+                    <p className='branch-text'>87 E BANK RD.CAINTA,1900 RIZAL PHILLIPINES.</p>
+                    <a
+                      href="https://maps.app.goo.gl/6CaZhrsiK4kpP4oS8"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn btn-outline-light"
+                    >
+                      View on Google Maps
+                    </a>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img src="../pinagbuhatan-branch.png" />
+                  <Carousel.Caption>
+                    <h3>Pinagbuhatan Branch</h3>
+                    <p className='branch-text'>63 Urbano Velasco Ave, Pasig, Metro Manila</p>
+                    <a
+                      href="https://maps.app.goo.gl/qbqsLzfkB4NBAPwCA"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn btn-outline-light"
+                    >
+                      View on Google Maps
+                    </a>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img src="../sanjoaquin-branch.png" />
+                  <Carousel.Caption>
+                    <h3>San Joaquin Branch</h3>
+                    <p className='branch-text'>8 Justice Ramon Jabson, Pasig, 1601 Metro Manila</p>
+                    <a
+                      href="https://maps.app.goo.gl/5sqrK6SdAwA1Qtuf7"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn btn-outline-light"
+                    >
+                      View on Google Maps
+                    </a>
                   </Carousel.Caption>
                 </Carousel.Item>
               </Carousel>
             </div>
-          </div>
         </Container>
     </Layout>
   )
