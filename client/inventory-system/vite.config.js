adf72f5ee0e6138c5d4d7efbef7b74e3e7f0c861
DerrickVal1256 <derrickmvaldellon@gmail.com>
2024-07-27 01:23:11 +0800

    Update vite.config.js

--- client/inventory-system/vite.config.js
+++ client/inventory-system/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:8001'
+      '/api': 'https://the-billy-project-deploy-1.onrender.com'
     }
   },
   plugins: [react()],
