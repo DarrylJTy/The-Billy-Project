d21f8ca4a1b5f52d729d180861f36d4ba1959fab
Hans Jared Sescon <hjcsescon@gmail.com>
2024-07-31 02:36:56 +0800

    About us complete
    - finished about us page
    - fixed custom footer

--- client/showcase-website/src/pages/about.tsx
+++ client/showcase-website/src/pages/about.tsx
@@ -11,29 +11,24 @@ export default function About() {
         
         <Container fluid className="about-container">
           <div className="content-container">
-            <div className='d-flex flex-row first-content-block'>
-              
-            {/* top content block */}
-              
+            <div className='d-flex flex-row about-us-content-block'>
+              <h2>About Us</h2>  
+            {/* top content block */} 
             </div>
 
             <div className='d-flex flex-row second-content-block'>
               <div className="col-sm bottom-area">
-                <div className="bottom-content">
-                    
-                {/* bottom left content block */}  
-                </div>
+                <img src="../about-us-img.jpg" className='img-fluid px-0 about-img' /> 
               </div>
 
               <div className="col-sm about-area">
                 <div className="px-3 about-content">
-                  <p className="text-justify">
-                  <h2>About Us</h2><br />
+                  <p className="about-us-text">
+                    <h2 className='about-us-title'>What we do</h2><br /> 
                     Welcome to Mr. Billy Build Easy, your go-to home improvement store located at Marcos Alvarez, 
                     Las Pinas City, Metro Manila. Since 2022, we've been committed to providing affordable and quality home fixtures. 
                     Visit us at our main branch or at our sub-branches in Pasig and Laguna
                   </p>
-                  
                 </div>
               </div>
             </div>
